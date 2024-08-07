155abdfffacc4b82bffc407f118b27d3735cb441
주강 <jkpark@nkia.co.kr>
2024-08-07 16:51:46 +0900

    update env key name

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -13,8 +13,8 @@ export const updateSession = async (request: NextRequest) => {
     });
 
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.NEXT_SUPABASE_URL!,
+      process.env.NEXT_SUPABASE_ANON_KEY!,
       {
         cookies: {
           getAll() {
@@ -22,17 +22,17 @@ export const updateSession = async (request: NextRequest) => {
           },
           setAll(cookiesToSet) {
             cookiesToSet.forEach(({ name, value }) =>
-              request.cookies.set(name, value),
+              request.cookies.set(name, value)
             );
             response = NextResponse.next({
               request,
             });
             cookiesToSet.forEach(({ name, value, options }) =>
-              response.cookies.set(name, value, options),
+              response.cookies.set(name, value, options)
             );
           },
         },
-      },
+      }
     );
 
     // This will refresh session if expired - required for Server Components
