6342994629378c84cf972c67a84ebed1478c932f
주강 <jkpark@nkia.co.kr>
2024-08-31 12:45:55 +0900

    각 버튼들 클릭했을때 로그인되지 않은 상태라면, login 페이지로 리다이렉트 기능
    - 나중에 함수 중복 방지할것임
    - 테스트 코드도 나중에 넣어야함 전부

--- src/app/components/common/CommentButton.tsx
+++ src/app/components/common/CommentButton.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 import { FaComment } from "react-icons/fa";
 
 const CommentButton = () => {
+  const router = useRouter();
   const [isHovered, setIsHovered] = useState(false);
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  const handleClick = useCallback(() => {
+    if (!isLoggedIn) {
+      alert("please sign in"); // 다국어 처리하기
+      return router.push("/login");
+    } else {
+      alert("Comment functionality is under development.");
+    }
+  }, [isLoggedIn, router]);
 
   return (
     <FaComment
       size={22}
       className={`cursor-pointer transition-colors duration-300`} // 색상 변경에 애니메이션 추가
-      color={isHovered ? "white" : "gray"}
+      color={isHovered && isLoggedIn ? "white" : "gray"}
+      onClick={handleClick}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     />
--- src/app/components/common/LikeButton.tsx
+++ src/app/components/common/LikeButton.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 import { FaThumbsUp } from "react-icons/fa";
 
 const LikeButton = () => {
+  const router = useRouter();
   const [isHovered, setIsHovered] = useState(false);
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  const handleClick = useCallback(() => {
+    if (!isLoggedIn) {
+      alert("please sign in"); // 다국어 처리하기
+      return router.push("/login");
+    } else {
+      alert("Like functionality is under development.");
+    }
+  }, [isLoggedIn, router]);
 
   return (
     <FaThumbsUp
       size={22}
       className={`cursor-pointer transition-colors duration-300`} // 색상 변경에 애니메이션 추가
-      color={isHovered ? "white" : "gray"}
+      color={isHovered && isLoggedIn ? "white" : "gray"}
+      onClick={handleClick}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     />
