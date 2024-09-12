a287f344bf9d86615001fab7d5612056618dd9ca
주강 <jkpark@nkia.co.kr>
2024-09-12 17:50:06 +0900

    [버그픽스] 채팅 메시지 두번 날아가는 버그 픽스
    - form 태그 추가
    - onSubmit 이벤트 를 기준으로 handleCreateMessage 함수 호출하여 메시지 전송하도록 변경

--- src/app/components/chat/MessageInput.tsx
+++ src/app/components/chat/MessageInput.tsx
@@ -11,33 +11,30 @@ const MessageInput = ({ loggedInUser }: MessageInputProps) => {
   const pathname = usePathname();
   const currentChatRoomId = pathname.split("/").pop(); // 현재 chatRoomId 추출
 
-  const handleKeyDown = async (
-    event: React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    if (event.key === "Enter") {
-      // Enter 키를 누르면 메시지를 전송
-
-      await createMessage(`${currentChatRoomId}`, loggedInUser.id, messageText);
-
-      setMessageText("");
-    }
+  const handleCreateMessage = async () => {
+    await createMessage(`${currentChatRoomId}`, loggedInUser.id, messageText);
+    setMessageText("");
   };
 
   return (
-    <div className="flex">
-      <input
+    <div className="flex w-full">
+      <form
+        className="block w-full"
         onSubmit={(e) => {
           e.preventDefault(); // 폼제출 막기
+          handleCreateMessage();
         }}
-        value={messageText} // 입력 값
-        onChange={(e) => {
-          setMessageText(e.target.value);
-        }} // 입력 값 업데이트
-        onKeyDown={handleKeyDown} // Enter 키 입력 처리
-        type="text"
-        className="flex-grow border rounded-lg p-2 dark:bg-gray-800 dark:text-white dark:border-gray-600"
-        placeholder="Type a message..."
-      />
+      >
+        <input
+          value={messageText} // 입력 값
+          onChange={(e) => {
+            setMessageText(e.target.value);
+          }} // 입력 값 업데이트
+          type="text"
+          className="w-full flex-grow border rounded-lg p-2 dark:bg-gray-800 dark:text-white dark:border-gray-600"
+          placeholder="Type a message..."
+        />
+      </form>
     </div>
   );
 };
