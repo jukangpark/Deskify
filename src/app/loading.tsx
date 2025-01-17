c3eb88a14aef5cebdac2dfebe6740f12111de8bd
주강 <jkpark@nkia.co.kr>
2025-01-17 13:46:26 +0900

    Nextjs 에서 제공하는 loading.tsx 를 활용하여, Suspense 를 대신하게 처리

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "./components/common/LoadingSpinner";
+
+export default function Loading() {
+  return (
+    <div className="mt-[24px]">
+      <LoadingSpinner />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
 import Posts from "./components/layout/Posts";
-import LoadingSpinner from "./components/common/LoadingSpinner";
+
 const Home = () => {
   return (
     <div className="mt-[24px]">
-      <Suspense fallback={<LoadingSpinner />}>
-        <Posts />
-      </Suspense>
+      <Posts />
     </div>
   );
 };
